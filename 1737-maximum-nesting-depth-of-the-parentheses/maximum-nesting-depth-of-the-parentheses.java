5d56e199cceb157a035e67c42188e9d1802062e3
weianc <55044671+weianc@users.noreply.github.com>
2024-01-28 17:29:25 -0800

    Time: 0 ms (100.00%) | Memory: 41.4 MB (47.58%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxDepth(String s) {
+        int left = 0;
+        int globalMax = 0;
+        boolean isEmptyStr = true;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '('){
+                left++;
+            }
+            else if (c == ')'){
+                left--;
+            }
+            else {// everything else
+                // update local num with globalMax
+                
+            }
+            globalMax = Math.max(globalMax, left);
+        }
+
+        return globalMax;
+    }
+}
