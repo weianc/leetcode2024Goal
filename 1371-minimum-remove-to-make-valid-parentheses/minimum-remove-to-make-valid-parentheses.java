c0488d3482fb5cb57e6cf66dba555f6522e723bd
weianc <55044671+weianc@users.noreply.github.com>
2024-03-16 01:06:19 -0700

    Time: 24 ms (43.31%) | Memory: 45.5 MB (31.88%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        // leet(t(c)o)de)
+        Stack<Integer> stk = new Stack();
+        int j = 0;
+        while(j < s.length()){
+            char c = s.charAt(j);
+            if(c == '('){
+                stk.push(j);
+            }
+            else if (c == ')'){
+                if(stk.isEmpty()){
+                    // remove current position from s directly
+                    s = s.substring(0, j) + s.substring(j+1, s.length());
+                    continue;
+                }
+                else {
+                    stk.pop();
+                }
+            }
+            j++;
+        }
+
+        String res = "";
+        int i = s.length();
+        if(stk.isEmpty())
+        {
+            return s;
+        }
+        while(!stk.isEmpty()){
+            int index = stk.pop();
+            String substring = s.substring(index+1, i); // do not include i
+            i = index;
+            res = substring + res;
+        }
+
+        if(i != 0){
+            res = s.substring(0, i) + res;
+        }
+
+        return res;
+    }
+}
