881b3d03067bab0829168d48ee7949eab683a27a
weianc <55044671+weianc@users.noreply.github.com>
2024-03-18 01:00:38 -0700

    Time: 25 ms (41.86%) | Memory: 46.1 MB (57.90%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -0,0 +1,52 @@
+
+
+import static java.lang.Math.log;
+
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stk = new Stack();
+        Set<Character> set = new HashSet<>();
+        set.add('+');
+        set.add('-');
+        set.add('*');
+        set.add('/');
+        char sign = '+';
+        int num = 0;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                // add to last number if there's one
+                num = num * 10 + c - '0';
+            }
+            // need to consider last char in string last char is isDigit
+            // or this will not be added into stack
+            if((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) // sign
+            {
+                int pre;
+                if(sign == '+'){
+                    stk.push(num);
+                }
+                else if (sign == '-'){
+                    stk.push(-num);
+                }
+                else if (sign == '*'){
+                    pre = stk.pop();
+                    stk.push(pre * num);
+                }
+                else if (sign == '/'){
+                    pre = stk.pop();
+                    stk.push(pre / num);
+                }
+                // reset num and sign
+                num = 0;
+                sign = c;
+            }
+        }
+        int res = 0;
+        // calculate total
+        while(!stk.isEmpty()){
+            res += stk.pop();
+        }
+        return res;
+    }
+}
