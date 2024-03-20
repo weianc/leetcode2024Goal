3675052cf4cab663cc9d6431cc87b086bdb1b882
weianc <55044671+weianc@users.noreply.github.com>
2024-03-19 22:12:22 -0700

    Time: 4 ms (99.68%) | Memory: 45 MB (93.07%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        // when finding not mismatch situation:
+        // left boundary: i, right boundary: j
+        // check if s[i+1, j] and s[i, j-1] is palindrome or not
+        int i = 0;
+        int j = s.length()-1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                // 如果s[i+1, j]和s[i,j-1]都不是palindrome
+                // 表示不符合情況
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+                // if(!isPalindrome(s, i+1, j) && !isPalindrome(s, i, j-1))
+                // {
+                //     return false;
+                // }
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    // 利用頭尾指針
+    private boolean isPalindrome(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
