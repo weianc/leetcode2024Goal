df2b7383b7ecc2569e9f567c3c4c7d572c9900a1
weianc <55044671+weianc@users.noreply.github.com>
2024-03-28 18:49:27 -0700

    Time: 25 ms (53.72%) | Memory: 46.6 MB (30.47%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -3,11 +3,11 @@ class Solution {
         int res = 0; 
         // 1. compose of hashmap
         Map<Integer, Integer> prefixSumCountMap = new HashMap<>();
-        int[] preSum = new int[nums.length + 1];
+        int prefixSum = 0;
         prefixSumCountMap.put(0, 1);
         for(int i = 0; i < nums.length; i++){
-            preSum[i+1] = preSum[i] + nums[i];
-            int prefixSum = preSum[i+1];
+            // preSum[i+1] = preSum[i] + nums[i];
+            prefixSum = prefixSum + nums[i];
             // 2. find all k - prefixSum = remain, if remain contains in map, then add to result
             if(prefixSumCountMap.containsKey(prefixSum - k)){
                 res += prefixSumCountMap.get(prefixSum - k);
