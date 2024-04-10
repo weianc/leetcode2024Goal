a232b6dfa2b742842033532792d9c4aca985de69
weianc <55044671+weianc@users.noreply.github.com>
2024-04-10 00:57:23 -0700

    Time: 0 ms (100.00%) | Memory: 44.4 MB (38.61%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        // 对于数据结构复制，甭管他怎么变，你就记住最简单的方式：一个哈希表 + 两次遍历。
+        // hashmap: 原始節點跟新節點的mapping
+        Map<Node, Node> nodeMapping = new HashMap<>();
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node cur = head;
+        // 第一輪: 複製節點
+        while(cur != null){
+            Node copy = new Node(cur.val);
+            nodeMapping.put(cur, copy);
+            cur = cur.next;
+        }
+
+        // 第二輪: 複製pointer
+        for(Node ori : nodeMapping.keySet()){
+            Node copyNode = nodeMapping.get(ori);
+            Node copyNext = nodeMapping.get(ori.next);
+            Node copyRandom = nodeMapping.get(ori.random);
+            copyNode.next = copyNext;
+            copyNode.random = copyRandom;
+        }
+
+        // return new head
+        return nodeMapping.get(head);
+    }
+}
