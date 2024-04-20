fafb54ee1ce1d9f976d59ff663134b6c03414344
weianc <55044671+weianc@users.noreply.github.com>
2024-04-20 00:14:08 -0700

    Time: 3 ms (74.04%) | Memory: 47.3 MB (30.41%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -1,46 +1,57 @@
 class Solution {
-    // Solution 1: sum of indices (extra space)
+    // Solution 2: Predict diagonal direction
     public int[] findDiagonalOrder(int[][] mat) {
-        // [1, 2, 3]
-        // [4, 5, 6]
-        // [7, 8, 9]
-        // On the same diagnal, the sum of indices are equal
-        // [1]: {0, 0} reverse
-        // [2]: {0, 1}, [4]{1, 0} 
-        // [3]: {0, 2}, [5]{1, 1}, [7]{2, 0} reverse: from large to small
-        // [6]: {1, 2}, [8]{2, 1}
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        // 1. diagonal: {r -1, c + 1}, (row + col) % 2 == 0
+        // 1.1 c == n-1: column should not increase, r + 1
+        // 1.2 r == 0: row should not decrease, c + 1
+        // 1.3 c < n-1 && r > 0: r - 1, c + 1
+        // 2. reverse diagonal: {r + 1, c - 1}, {row + col} % 2 == 1
+        // 2.1: r == m - 1: // 下边到底了，要换方向，坐标(r, c+1), 
+        // row should not increase, c + 1
+        // 2.2: c == 0: // 左边到底了，要换方向，坐标(r+1, c)
+        // col should not decrease, r + 1
+        // 2.3: c > 0 && r < m-1: {r+1, c-1}
         int m = mat.length;
         int n = mat[0].length;
-        int[] ans = new int[n*m];
-        for(int r = 0; r < m; r++){
-            for(int c = 0; c < n; c++){
-                int num = mat[r][c];
-                if(!map.containsKey(r + c)){
-                    map.put(r+c, new ArrayList<>());
+        int[] res = new int[m * n];
+        int r = 0;
+        int c = 0;
+        for(int i = 0; i < res.length; i++){
+            // assign to res first
+            res[i] = mat[r][c];
+            int sum = r + c;
+            // 1. diagonal {r - 1, c + 1} 
+            if(sum % 2 == 0){
+                if(c == n - 1){
+                    // 已經到右邊界，換方向r+1
+                    r += 1;
                 }
-                map.get(r+c).add(num);
-            }
-        }
-
-        // iterate through map and then build res
-        int count = 0;
-        for(Integer key : map.keySet()){
-            List<Integer> numList = map.get(key);
-            // if key % 2 == 0, reverse sequence
-            if(key % 2 == 0){
-                for(int i = numList.size() - 1; i >= 0; i--){
-                    ans[count] = numList.get(i);
-                    count++;
+                else if (r == 0){
+                    // 已經到上邊界，換方向c+1
+                    c += 1;
+                }
+                else {
+                    // r,c都在邊界
+                    r -= 1;
+                    c += 1;
                 }
             }
+            // 2. reverse diagonal {r + 1, c - 1} 
             else {
-                for(int i = 0; i < numList.size(); i++){
-                    ans[count] = numList.get(i);
-                    count++;
+                if(r == m - 1){
+                    // 已經到下邊界，換方向 c + 1
+                    c += 1;
+                }
+                else if (c == 0){
+                    // 已經到左邊界，換方向r+1
+                    r += 1;
+                }
+                else {
+                    r += 1;
+                    c -= 1;
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
