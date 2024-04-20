8344704c5b9d1af4ba3db6534f5fe5921e3cdc6c
weianc <55044671+weianc@users.noreply.github.com>
2024-04-20 14:09:03 -0700

    Time: 0 ms (100.00%) | Memory: 44.1 MB (61.02%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,39 +1,25 @@
 class Solution {
-    // Solution 1: compare with Down-Right neighbor
+    // Solution 2: only compare to left-top neighbor
     public boolean isToeplitzMatrix(int[][] matrix) {
         // [m, n] -> [m+1, n+1]
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean[][] visited = new boolean[m][n];
+        
         for(int r = 0; r < m; r++){
             for(int c = 0; c < n; c++){
-                if(!visited[r][c]){
-                    if(!isSameOnDiagonal(r, c, m, n, matrix, visited)){
-                        return false;
-                    };
+                // left top neighbor
+                int pr = r - 1;
+                int pc = c - 1;
+                if(isBounded(pr, pc, m, n) && matrix[r][c] != matrix[pr][pc]){
+                    return false;
                 }
             }
         }
         return true;
     }
 
-    private boolean isSameOnDiagonal(int r, int c, int m, int n, int[][] matrix, boolean[][] visited){
-        int target = matrix[r][c];
-        while(isBounded(r, c, m, n)) {
-            visited[r][c] = true;
-            if(matrix[r][c] != target){
-                return false;
-            }
-            r = r + 1;
-            c = c + 1;
-        }
-        return true;
-    }
-
     private boolean isBounded(int r, int c, int m, int n){
-        if(r >= 0 && r < m && c >= 0 && c < n){
-            return true;
-        }
+        if(r >= 0 && r < m && c >= 0 && c < n) return true;
         return false;
     }
 }
