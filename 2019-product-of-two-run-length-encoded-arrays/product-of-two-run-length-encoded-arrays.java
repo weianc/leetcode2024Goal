187ccf8a76dce92cac1d5dabc6655aa7b83d0620
weianc <55044671+weianc@users.noreply.github.com>
2024-04-22 18:16:04 -0700

    Time: 15 ms (99.78%) | Memory: 84 MB (83.21%) - LeetSync

---
+++ 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
+        List<List<Integer>> prodNums = new ArrayList<>();
+        
+        int i = 0, j = 0; // Encoded1 和 Encoded2 的索引
+        int m = encoded1.length, n = encoded2.length;
+        int totalLength = 0; // 兩個 encoded 陣列的總長度
+        
+        // 計算兩個 encoded 陣列的總長度
+        for (int k = 0; k < m; k++) {
+            totalLength += encoded1[k][1];
+        }
+        for (int k = 0; k < n; k++) {
+            totalLength += encoded2[k][1];
+        }
+
+        int prevProdNum = -1; // 上一個產品數
+        int count = 0; // 用於計算連續相同產品數的計數器
+
+        while (i < m && j < n) {
+            int num1 = encoded1[i][0], freq1 = encoded1[i][1];
+            int num2 = encoded2[j][0], freq2 = encoded2[j][1];
+            int curProdNum = num1 * num2; // 目前的產品數
+
+            // 更新迴圈索引
+            int minFreq = Math.min(freq1, freq2);
+            encoded1[i][1] -= minFreq;
+            encoded2[j][1] -= minFreq;
+            if (encoded1[i][1] == 0) i++;
+            if (encoded2[j][1] == 0) j++;
+
+            // 更新結果
+            if (curProdNum == prevProdNum) {
+                count += minFreq;
+            } else {
+                if (prevProdNum != -1) {
+                    prodNums.add(Arrays.asList(prevProdNum, count));
+                }
+                prevProdNum = curProdNum;
+                count = minFreq;
+            }
+        }
+
+        // 將最後一個結果添加到列表中
+        if (prevProdNum != -1) {
+            prodNums.add(Arrays.asList(prevProdNum, count));
+        }
+
+        return prodNums;
+    }
+}
