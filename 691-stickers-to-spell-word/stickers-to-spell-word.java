13615d6a62a4a1379fbbd7fc184d6b95156d5600
weianc <55044671+weianc@users.noreply.github.com>
2024-05-04 00:25:20 -0700

    Time: 82 ms (59.05%) | Memory: 45.6 MB (26.68%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.java
+++ 691-stickers-to-spell-word/stickers-to-spell-word.java
@@ -1,9 +1,8 @@
 
 
 class Solution {
-    int n;
     public int minStickers(String[] stickers, String target) {
-        n = target.length(); // 每個char都要用一張貼紙
+        int n = target.length(); // 每個char都要用一張貼紙
         Map<String, Map<Character, Integer>> stickerCharCountMap = new HashMap<>();
         for(String s : stickers){
             Map<Character, Integer> charCount = new HashMap<>();
@@ -15,13 +14,14 @@ class Solution {
         }
 
         //Map<String, Integer> memo = new HashMap<>();
-        int res = DFS(stickers, target, stickerCharCountMap, new HashMap<>());
+        int res = DFS(stickers, target, stickerCharCountMap, new HashMap<>(), n);
         return res > target.length() ? -1 : res;
     }
 
     private int DFS(String[] stickers, String targetStr,
         Map<String, Map<Character, Integer>> stickerCharCountMap,
-        Map<String, Integer> memo){
+        Map<String, Integer> memo,
+        int n){
         // 遍歷貼紙查看是否有第一個char match target String
         // 1. 有match current first char: 遍歷當前貼紙，並且用盡所有可以用的單字
         // 2. 略過
@@ -37,18 +37,19 @@ class Solution {
         int res = n + 1;
         // (1)with, (2)example, (3)science
         for(String s : stickers){
-            Map<Character, Integer> tmpCharCount = new HashMap(stickerCharCountMap.get(s));
-            String currentStr = targetStr;
-            char c = currentStr.charAt(0);
-            if(!tmpCharCount.containsKey(c)) continue;
+            Map<Character, Integer> tmpCharCount = new HashMap<>(stickerCharCountMap.get(s));
+            char firstChar = targetStr.charAt(0);
+            if(!tmpCharCount.containsKey(firstChar)) continue;
             // iterate through current String and remove match word in sticker s
-            for(int i = 0; i < currentStr.length(); i++){
-                c = currentStr.charAt(i);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < targetStr.length(); i++){
+                char c = targetStr.charAt(i);
                 if(tmpCharCount.containsKey(c) && tmpCharCount.get(c) > 0){
                     tmpCharCount.put(c, tmpCharCount.get(c) - 1);
-                    currentStr = currentStr.substring(0, i) + currentStr.substring(i+1);
-                    // decrement as current character is removed
-                    i--;
+                    //currentStr = currentStr.substring(0, i) + currentStr.substring(i+1);
+                }
+                else {
+                    sb.append(c);
                 }
             }
             // last step: if currentStr is modified,
@@ -56,8 +57,8 @@ class Solution {
             // then update memo table
             // 1 more sticker was used.
             // currentStr: ehat, targetStr: thehat
-            if(currentStr.length() != targetStr.length()){
-                res = Math.min(res, 1 + DFS(stickers, currentStr, stickerCharCountMap, memo));
+            if(sb.length() != targetStr.length()){
+                res = Math.min(res, 1 + DFS(stickers, sb.toString(), stickerCharCountMap, memo, n));
                 // update memo table
                 memo.put(targetStr, res);
             }
