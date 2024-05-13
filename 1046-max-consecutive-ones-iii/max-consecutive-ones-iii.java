855405af1b49c75e0b2694f685660a90fa792e77
weianc <55044671+weianc@users.noreply.github.com>
2024-05-13 15:58:31 -0700

    Time: 4 ms (26.05%) | Memory: 47.3 MB (20.62%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,6 +3,7 @@ class Solution {
         // 最長子陣列問題通常會使用滑動窗口解決
         int left = 0;
         int right = 0;
+        // 紀錄窗口中1的出現次數
         int sum = 0;
         int globalMax = 0;
         // shrink window condition: right - left > k + sum
@@ -14,7 +15,8 @@ class Solution {
             right++;
 
             // 2. shrink window
-            // need to increase left pointer
+            // 當窗口中需要替換的 0 > k，縮小窗口
+            // 超過可以替換或是連續的1數目
             while(right - left > k + sum){
                 if(nums[left] == 1){
                     sum--;
