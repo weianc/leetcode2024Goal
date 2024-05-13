69d8ee53859f36e1a057d271b27bf64118487a66
weianc <55044671+weianc@users.noreply.github.com>
2024-05-13 16:40:51 -0700

    Time: 37 ms (5.50%) | Memory: 64.9 MB (5.16%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        // nums = [23, 2, 4, 6, 7]
+        // prefixSum = [0, 23, 25, 29, 35, 42]
+        // PSum[3] - PSum[1] = 29 - 23 = 6 (sum[1,2])
+        // Sum[0,3] - Sum[0,1]
+        // prefixSum%k vs 索引的映射
+        Map<Integer, Integer> psumModKToIndex = new HashMap<>();
+        // 1. create prefixSum
+        int[] psum = new int[nums.length + 1];
+        for(int i = 1; i < psum.length; i++){
+            psum[i] = psum[i-1] + nums[i-1];
+        }
+
+        for(int i = 0; i < psum.length; i++){
+            int val = psum[i] % k;
+            if(!psumModKToIndex.containsKey(val)){
+                psumModKToIndex.put(val, i);
+            }
+        }
+
+        for(int i = 0; i < psum.length; i++){
+            // 計算need，找出psum[i] % k已經在hashmap中出現過的結果
+            // 因為可以得出一樣的餘數表示數字相差至少是6
+            int need = psum[i] % k; 
+            if(psumModKToIndex.containsKey(need)){
+                int index = psumModKToIndex.get(need);
+                // 並且計算出pSum index差值至少是2
+                int length = Math.abs(index - i);
+                if(length >= 2){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
