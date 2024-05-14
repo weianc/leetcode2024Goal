267f39c0b524239d6d0a716ad5374f5408a04e89
weianc <55044671+weianc@users.noreply.github.com>
2024-05-14 15:48:49 -0700

    Time: 0 ms (100.00%) | Memory: 42.2 MB (48.45%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,22 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n == 0) return 1;
-        if(n == Integer.MIN_VALUE){
-            // -(n+1) == MAX
-            return myPow(1/x, Integer.MAX_VALUE) / x; 
+        if(n == 0) return 1; 
+        if(n == Integer.MIN_VALUE) {
+            // 會多乘一次
+            return myPow(1/x, Integer.MAX_VALUE) / x;
         }
-        if(n < 0){
-            return myPow(1/x, -n);
-        }
-
-        if(n % 2 == 0){ // n是偶數: a^n = a^n/2 * a^n/2
+        if(n < 0) return myPow(1/x, -n);
+        if(n % 2 == 0) {
             double sub = myPow(x, n/2);
             return sub * sub;
         }
         else {
-            // n是奇數: a^n = a * a^(n-1)
-            return x * myPow(x, n-1);
+            return myPow(x, n-1) * x;
         }
-        
     }
 }
