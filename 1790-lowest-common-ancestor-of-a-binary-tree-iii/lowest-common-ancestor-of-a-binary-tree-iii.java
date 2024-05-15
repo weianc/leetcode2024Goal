38141cddd66136b8882130ee9c804e349c07b646
weianc <55044671+weianc@users.noreply.github.com>
2024-05-15 15:50:15 -0700

    Time: 28 ms (82.21%) | Memory: 44.1 MB (90.15%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -9,6 +9,7 @@ class Node {
 */
 
 class Solution {
+    // 这道题其实不是公共祖先的问题，而是单链表相交的问题
     public Node lowestCommonAncestor(Node p, Node q) {
         Node p1 = p;
         Node p2 = q;
@@ -16,6 +17,8 @@ class Solution {
         while(p1.val != p2.val){
             p1 = p1.parent;
             p2 = p2.parent;
+            // 相遇點：經過這樣的操作，p1 和 p2 會在 LCA 處相遇。
+            // 這是因為兩個指針都會遍歷相同數量的節點：p 到 LCA 的路徑加上 q 到 LCA 的路徑。
             if(p1 == null){
                 p1 = q;
             }
