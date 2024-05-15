33f6f37454835d88067be8b3f819e0bb3967ec59
weianc <55044671+weianc@users.noreply.github.com>
2024-05-15 14:53:25 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (90.69%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -2,32 +2,25 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m-1;
         int p2 = n-1;
-        int i = m+n-1;
+        int i = nums1.length - 1;
 
-        while((p1 >= 0 || p2 >= 0) && i >= 0){
-            while(p1 >= 0 && p2 >= 0){
-                if(nums1[p1] > nums2[p2]){
-                    nums1[i] = nums1[p1];
-                    p1--;
-                }
-                else {
-                    nums1[i] = nums2[p2];
-                    p2--;
-                }
-                i--;
-            }
-
-            while(p1 >= 0){
+        while(p1 >= 0 && p2 >= 0){
+            if(nums1[p1] > nums2[p2]){
                 nums1[i] = nums1[p1];
                 p1--;
-                i--;
             }
-
-            while(p2 >= 0){
+            else {
                 nums1[i] = nums2[p2];
                 p2--;
-                i--;
             }
+            i--;
+        }
+
+        //  不用考慮p1還沒到結尾的情況，因為我們本身就是在修改nums1
+        while(p2 >= 0){
+            nums1[i] = nums2[p2];
+            p2--;
+            i--;
         }
     }
 }
