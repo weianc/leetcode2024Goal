1d310f8d9382d9c166766524c52ba8a2cfacc17e
weianc <55044671+weianc@users.noreply.github.com>
2024-05-16 14:57:48 -0700

    Time: 1 ms (33.79%) | Memory: 40.8 MB (5.65%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,21 +1,26 @@
 class Solution {
     public int maximumSwap(int num) {
         char[] digits = Integer.toString(num).toCharArray();
-        int[] lastPosition = new int[10];
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        //int[] lastPosition = new int[10];
 
         for(int i = 0; i < digits.length; i++){
             int number = digits[i] - '0';
-            lastPosition[number] = i;
+            //lastPosition[number] = i;
+            // 如果同一個數字有2個位置，選擇最後的位置
+            numToIndex.put(number, i);
         }
 
         for(int i = 0; i < digits.length; i++){
             int curNum = digits[i] - '0';
+            // find from the numToIndex map to see if corresponding position > i
             for(int n = 9; n > curNum; n--){
-                if(lastPosition[n] > i){
+                if(numToIndex.containsKey(n) && numToIndex.get(n) > i){
+                    int largeNumIndex = numToIndex.get(n);
                     char tmp = digits[i];
-                    digits[i] = digits[lastPosition[n]];
-                    digits[lastPosition[n]] = tmp;
-                    return Integer.parseInt(new String(digits));
+                    digits[i] = digits[largeNumIndex];
+                    digits[largeNumIndex] = tmp;
+                    return Integer.parseInt(String.valueOf(digits));
                 }
             }
         }
