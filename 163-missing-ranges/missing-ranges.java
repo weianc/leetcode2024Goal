3702f1070613ce5db3786df4fb22177414b6ec6e
weianc <55044671+weianc@users.noreply.github.com>
2024-05-16 16:09:35 -0700

    Time: 0 ms (100.00%) | Memory: 41.3 MB (60.12%) - LeetSync

--- 163-missing-ranges/missing-ranges.java
+++ 163-missing-ranges/missing-ranges.java
@@ -1,42 +1,31 @@
 class Solution {
     public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
-        List<Integer> copy = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
-        if(nums.length == 0){
-            List<Integer> range = new ArrayList<>();
-            range.add(lower);
-            range.add(upper);
-            res.add(range);
-            return res;
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Handle the range before the first element
+        if (nums.length == 0) {
+            // If there are no numbers, the missing range is the whole range
+            result.add(Arrays.asList(lower, upper));
+            return result;
         }
-        int n = nums.length;
-        copy.add(lower);
-        for(int i = 0; i < nums.length; i++){
-            copy.add(nums[i]);
-        }
-        copy.add(upper);
         
-        for(int i = 0; i < copy.size() - 1; i++){
-            int cur = copy.get(i);
-            int next = copy.get(i+1);
-
-            if(cur + 1 == next && i != 0 && i != copy.size()-2 || cur == next) continue;
-            List<Integer> range = new ArrayList<>();
-            if(i == 0){
-                range.add(cur);
-            }
-            else {
-                range.add(cur + 1);
-            }
-
-            if(i == copy.size() - 2){
-                range.add(next);
-            }
-            else {
-                range.add(next - 1);
-            }
-            res.add(range);
+        // Handle the initial missing range
+        if (nums[0] > lower) {
+            result.add(Arrays.asList(lower, nums[0] - 1));
         }
-        return res;
+        
+        // Handle the missing ranges between the numbers
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1] + 1) {
+                result.add(Arrays.asList(nums[i - 1] + 1, nums[i] - 1));
+            }
+        }
+        
+        // Handle the missing range after the last element
+        if (nums[nums.length - 1] < upper) {
+            result.add(Arrays.asList(nums[nums.length - 1] + 1, upper));
+        }
+        
+        return result;
     }
 }
