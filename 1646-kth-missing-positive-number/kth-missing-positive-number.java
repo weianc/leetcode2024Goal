8653d7dedf2b9767111571a9342e88d2fb44ae4e
weianc <55044671+weianc@users.noreply.github.com>
2024-05-16 21:45:28 -0700

    Time: 0 ms (100.00%) | Memory: 42.8 MB (41.08%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,28 +1,24 @@
 class Solution {
-    // Solution: linear search
-    // [2, 3, 4, 6, 11]
+    // Solution 2: binary search
     public int findKthPositive(int[] arr, int k) {
-        int j = 0;
-        int n = arr.length;
-        int curNum = 1;
-        int count = 0;
-        while(count < k && j < n){
-            if(curNum == arr[j]){
-                // count do not need to move
-                j++;
+        int l = 0;
+        int r = arr.length-1;
+
+        // the answer is between arr[right] and arr[right]
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            // missing before mid smaller than k
+            if(arr[mid] - (mid + 1) < k){
+                l = mid + 1;
             }
             else {
-                // means that curNum is missing from arr
-                count++;
+                r = mid - 1;
             }
-
-            // check count == k or not 
-            if (count == k) return curNum;
-            curNum++;
         }
 
-        // arr is done but k is not yet reached
-        // k - count: number that missing from array
-        return curNum - 1+ k - count;
+        // l = r + 1
+        // r is the index which arr[r] - r - 1 < k
+        // int remaining = k -(arr[r] - r - 1);
+        return l + k;
     }
 }
